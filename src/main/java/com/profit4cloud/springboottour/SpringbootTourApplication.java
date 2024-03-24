8f3cfcd131011b616020dc278a3733f7c14c5bda
MurtazaAydogdu95 <Murtaza.Aydogdu@theapsgroup.com>
2024-03-24 19:18:38 +0100

    setup

---
+++ src/main/java/com/profit4cloud/springboottour/SpringbootTourApplication.java
@@ -0,0 +1,13 @@
+package com.profit4cloud.springboottour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTourApplication.class, args);
+    }
+
+}
