b089b01729c426c7441c3cf879bc72f816180259
MurtazaAydogdu95 <Murtaza.Aydogdu@theapsgroup.com>
2024-04-28 22:37:32 +0200

    added eks

---
+++ infra/eks_node_group.tf
@@ -0,0 +1,73 @@
+resource "aws_instance" "kubectl-server" {
+  ami                         = "ami-019a292cfb114a776"
+  key_name                    = aws_key_pair.generated.key_name
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public-1.id
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "kubectl"
+  }
+
+  connection {
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
+
+}
+
+resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "pc-node-group"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
+  capacity_type   = "ON_DEMAND"
+  disk_size       = "20"
+  instance_types  = ["t2.small"]
+
+  remote_access {
+    ec2_ssh_key               = aws_key_pair.generated.key_name
+    source_security_group_ids = [aws_security_group.allow_tls.id]
+  }
+
+  labels = tomap({ env = "dev" })
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    #aws_subnet.pub_sub1,
+    #aws_subnet.pub_sub2,
+  ]
+}
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "springboot.pem"
+}
+
+resource "aws_key_pair" "generated" {
+  key_name   = "springboot"
+  public_key = tls_private_key.generated.public_key_openssh
+
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
+
--- infra/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ecs_cluster_id" {
-  description = "ID of the ECS cluster"
-  value       = module.ecs.ecs_cluster_id
-}
